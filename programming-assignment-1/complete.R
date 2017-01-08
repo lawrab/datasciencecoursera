e232b213547d19ceafbc23ed54a8585b72b16d24
lawrab <lawrab@users.noreply.github.com>
2017-01-08 22:02:13 +0000

    Renaming column for queastions

--- programming-assignment-1/complete.R
+++ programming-assignment-1/complete.R
@@ -1,12 +1,12 @@
 complete <- function(directory, id = 1:332) {
     fnames <- sprintf("%03d.csv", id)
     files <- file.path(directory, fnames)
-    nocc <- integer()
+    nobs <- integer()
     
     for(f in files){
         data <- read.csv(f)
-        nocc <- c(nocc, nrow(data[complete.cases(data),]))
+        nobs <- c(nobs, nrow(data[complete.cases(data),]))
     }
     
-    data.frame(fnames, nocc, stringsAsFactors=FALSE)
+    data.frame(fnames, nobs, stringsAsFactors=FALSE)
 }
